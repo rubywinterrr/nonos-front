1cd210a36f28fe8f43e1e0664dd230a4d317555f
Joaquin Schopflocher <joacoschop@gmail.com>
2026-08-18 12:03:17 -0300

    creacion carpetas iniciales

---
+++ nonos-front/src/Panel-Adulto/main.tsx
@@ -0,0 +1,11 @@
+// 1. Conectas el archivo CSS
+import './main.css';
+
+// 2. Defines y exportas el componente de tu pantalla
+export const MiPantalla = () => {
+  return (
+    <div className="contenedor-pantalla">
+      <h1>Hola desde mi pantalla</h1>
+    </div>
+  );
+};
